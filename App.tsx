0938362e4c415240e31cafa72ab0425fcb1dc897
Lê Thị Thuỷ Tiên <1204thuytien@gmail.com>
2024-03-24 22:06:15 +0700

    add

--- App.tsx
+++ App.tsx
@@ -1,118 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Tts from 'react-native-tts'
+interface textProps {
+  id: number,
+  word: string
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
 }
+const App = () => {
+  const [ttsInitialized, setTtsInitialized] = useState(false); // Track TTS initialization
+  const [selectedLanguage, setSelectedLanguage] = useState('vi');
+  const [rate, setRate] = useState(1); // Tốc độ đọc
+  const [volume, setVolume] = useState(1); // Âm lượng
+  const languages = {
+    vi: 'vi-VN',
+    en: 'en-US',
+  };
+  const itemnText = ({ item }: { item: textProps }) => {
+    return (
+      <Pressable style={{
+        width: '100%', height: 40,
+        backgroundColor: "yellow", marginBottom: 16
+      }} onPress={() => speakText(item.word)}>
+        <Text>{item.word}</Text>
+      </Pressable>
+    )
+  }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const initializeTts = async () => {
+    const result = await Tts.getInitStatus(); //lấy trạng thái trong tts dâc chuyển đổi thành công sang giọng nói chưa
+    if (result === 'success') {
+      //nếu thành công sẽ xét trạng thái thành true
+      setTtsInitialized(true);
+    }
+  };
+  useEffect(() => {
+    initializeTts();
+  }, []);
+  const speakText = (word: string) => {
+    if (ttsInitialized) {
+      Tts.speak(word
+      );
+    } else {
+      console.log('TTS engine not yet initialized. Text will not be spoken.');
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <FlatList
+      data={data}
+      renderItem={itemnText} />
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+export default App
 
-export default App;
+const styles = StyleSheet.create({})
+
+const data = [
+  {
+    id: 1,
+    word: 'Cow'
+  },
+  {
+    id: 2,
+    word: 'Chicken'
+  },
+  {
+    id: 3,
+    word: 'Bee'
+  },
+  {
+    id: 4,
+    word: 'Wolk'
+  },
+  {
+    id: 5,
+    word: 'Bird'
+  },
+  {
+    id: 6,
+    word: 'Sheep'
+  },
+
+  {
+    id: 7,
+    word: ' Earthworm'
+  },
+]
